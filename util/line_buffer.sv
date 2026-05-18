b8ef7381ccba6f5a20762189e1c30c790bfa09eb
david <david88gao@gmail.com>
2026-05-18 15:18:02 -0400

    init commit + param'd line_buffer
    Co-authored-by: haydenkan <haydenkan@users.noreply.github.com>

---
+++ util/line_buffer.sv
@@ -0,0 +1,84 @@
+module line_buffer #(
+   parameter DATA_WIDTH  = 8,
+   parameter LINE_WORDS  = 10,
+   parameter NUM_LINES   = 5
+)(
+   input  wire                  clk,
+   input  wire                  rst,
+   input  wire                  en,
+   input  wire [DATA_WIDTH-1:0] din,
+
+   output wire [DATA_WIDTH-1:0] dout_tap [NUM_LINES-2:0],
+   output reg [DATA_WIDTH-1:0] dout_tapn ,
+   //output wire                  dout_valid
+   output wire                  dout_padded_valid
+);
+
+   localparam ADDR_WIDTH = $clog2 (LINE_WORDS);
+   localparam BLOCK_WIDTH = $clog2 (NUM_LINES);
+   
+   reg  [ADDR_WIDTH-1:0] addr;
+   reg   [BLOCK_WIDTH-1:0] block_sel;
+   reg [ADDR_WIDTH+1:0]  latency_count;
+   //reg                   latency_done;
+   reg                   latency_padded_done;
+   reg                   en_r;
+   
+   //assign dout_valid        = en_r && latency_done;
+   assign dout_padded_valid = en_r && latency_padded_done;
+
+   generate
+      genvar i;
+      for (i = 1; i < NUM_LINES - 1; ++i) begin: cock
+         true_dual_port_ram2 #(
+            .DATA_WIDTH    (DATA_WIDTH),
+            .ADDR_WIDTH    (ADDR_WIDTH+BLOCK_WIDTH),
+            .WRITE_MODE_1  ("READ_FIRST"),
+            .WRITE_MODE_2  ("READ_FIRST"),
+            .OUTPUT_REG_1  ("FALSE"),
+            .OUTPUT_REG_2  ("FALSE"),
+            .RAM_INIT_FILE ("")
+         ) u_ram (
+            .we1   (en),
+            .clka  (clk),
+            .din1  (din),
+            .addr1 ({block_sel + i, addr}),
+            .dout1 (dout_tap[i]),
+
+            .we2   (1'b0),
+            .clkb  (clk),
+            .din2  ({DATA_WIDTH{1'b0}}),
+            .addr2 ({block_sel + i - 1, addr}),
+            .dout2 (dout_tap[i - 1])
+         );
+      end
+   endgenerate
+   
+
+   always_ff @(posedge clk or posedge rst)
+   begin
+      if (rst) begin
+         dout_tapn           <= {DATA_WIDTH{1'b0}};
+         addr                <= {ADDR_WIDTH{1'b0}};
+         block_sel           <= 1'b0;
+         latency_count       <= {ADDR_WIDTH+2{1'b0}};
+         //latency_done        <= 1'b0;
+         latency_padded_done <= 1'b0;
+         en_r                <= 1'b0;
+      end else begin
+         dout_tapn           <= din;
+         addr                <= (en && (addr==LINE_WORDS-1)) ? {ADDR_WIDTH{1'b0}} :
+                                (en)                         ? addr + {{ADDR_WIDTH-1{1'b0}}, 1'b1}
+                                                             : addr;
+         //make readable later
+         block_sel           <= (en && (addr==LINE_WORDS-1)) ? 
+                                 (block_sel==NUM_LINES) ? 1'b0 : block_sel + 1 : block_sel;
+         latency_count       <= (en) ? latency_count + {{ADDR_WIDTH+1{1'b0}}, 1'b1} : latency_count;
+         //latency_done        <= ((latency_count == (LINE_WORDS*2)) && en) ? 1'b1 : latency_done;        //LINE_COUNT*M where M =(kernel_height-1)/2 + 1;
+         latency_padded_done <= ((latency_count == (LINE_WORDS*1)) && en) ? 1'b1 : latency_padded_done;   //LINE_COUNT*M where M =(kernel_height-1)/2;
+         en_r                <= en;
+      end
+   end
+  
+endmodule
+
