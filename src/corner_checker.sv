6f9484f982a87aec894de1210428636cf3464298
haydenkan <kan.hayden@outlook.com>
2026-05-20 08:59:54 -0400

    commented corner checker
    Co-authored-by: davvdf <davvdf@users.noreply.github.com>

--- src/corner_checker.sv
+++ src/corner_checker.sv
@@ -1,24 +1,35 @@
+/** Corner candidate finder as a part of FAST algorithm */
 module corner_checker
 #(
-    parameter THRESHOLD = 10,
+    /** Threshold: dividng threshold to determine if contiguous pixels are similar 
+      * brightness level
+      */
+    parameter THRESHOLD = 6,
     parameter DATA_WIDTH = 8,
     parameter CIRCUMFERENCE = 16,
     parameter CONTIGUOUS_PIXELS = 9
 )
 (
+    /** Candidate pixel to center checking around */
     input logic [DATA_WIDTH-1:0] candidate,
+    /** Contiguous pixels to check brightness against candidate pixel*/
     input logic [CIRCUMFERENCE-1:0][DATA_WIDTH-1:0] adjacent,
     output logic is_corner
 );
 
-
+/** Calculate min/max intensity to compare against adjacent pixels 
+  * Over/underflow guarding prevents flipping of extreme brightness 
+  * values to the other end of the spectrum
+  * i.e. 4 - threshold = 250, which is not the intended behavior
+  */
 wire [DATA_WIDTH:0] thresh_dark_tmp = {1'b1, candidate} - THRESHOLD; 
 wire [DATA_WIDTH:0] thresh_light_tmp = {1'b0, candidate} + THRESHOLD;
-
 wire [DATA_WIDTH-1:0] thresh_dark = {DATA_WIDTH{thresh_dark_tmp[DATA_WIDTH]}} & thresh_dark_tmp[DATA_WIDTH-1:0];
 wire [DATA_WIDTH-1:0] thresh_light = {DATA_WIDTH{thresh_light_tmp[DATA_WIDTH]}} | thresh_light_tmp[DATA_WIDTH-1:0];
 
-
+/** Track whether each adjacent pixel is "darker" or "lighter" than the candidate pixel 
+  * (or too close in intensity, denoted by logic 0)
+  */
 wire [CIRCUMFERENCE-1:0] darker;
 wire [CIRCUMFERENCE-1:0] lighter;
 for (genvar i = 0; i < CIRCUMFERENCE; i++) begin: hello2
@@ -26,6 +37,9 @@ for (genvar i = 0; i < CIRCUMFERENCE; i++) begin: hello2
     assign lighter[i] = adjacent[i] > thresh_light;
 end
 
+/** Syntatic sugar to make it easy to perform reduction operation on consecutive pixels 
+  * Probably synthesizes to optimal logic idk
+  */
 wire [CIRCUMFERENCE*2-1:0] a = { darker, darker };
 wire [CIRCUMFERENCE*2-1:0] b = { lighter, lighter };
 
